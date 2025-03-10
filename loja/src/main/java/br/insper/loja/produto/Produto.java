4041850e7f16008ffd6a7fa60d6321d0f9661d00
pedrofardin <pedro.fardin@hotmail.com>
2025-03-10 17:17:07 -0300

    loja

---
+++ loja/src/main/java/br/insper/loja/produto/Produto.java
@@ -0,0 +1,54 @@
+package br.insper.loja.produto;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "produtos")
+public class Produto {
+
+    @Id
+    private String id;
+    private String nome;
+    private double preco;
+    private int quantidadeEstoque;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public void diminuirEstoque(int quantidade) {
+        if (this.quantidadeEstoque >= quantidade) {
+            this.quantidadeEstoque -= quantidade;
+        } else {
+            throw new IllegalArgumentException("Quantidade em estoque insuficiente.");
+        }
+    }
+}
