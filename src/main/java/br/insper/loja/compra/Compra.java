a18de1156cef9d593674a6e265555c92562d1fef
pedrofardin <pedro.fardin@hotmail.com>
2025-03-10 22:56:09 -0300

    fix

---
+++ src/main/java/br/insper/loja/compra/Compra.java
@@ -0,0 +1,67 @@
+package br.insper.loja.compra;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Document
+public class Compra {
+
+    @Id
+    private String id;
+    private String usuario;
+    private String nome;
+    private List<String> produtos;
+    private LocalDateTime dataCompra;
+    private double precoTotal;  // Novo campo
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<String> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<String> produtos) {
+        this.produtos = produtos;
+    }
+
+    public LocalDateTime getDataCompra() {
+        return dataCompra;
+    }
+
+    public void setDataCompra(LocalDateTime dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+
+    public double getPrecoTotal() {
+        return precoTotal;
+    }
+
+    public void setPrecoTotal(double precoTotal) {
+        this.precoTotal = precoTotal;
+    }
+}
